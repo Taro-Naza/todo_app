bbff44088f5b630ddb3bc87ea9fbd9ca98ef0dab
Taro Naza <maza.tarek@gmail.com>
2021-03-07 15:01:49 +0100

    Added the add-task form and added the local state to track the input and added some css to add some responiseness to the app

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import happy from '../assets/images/happy.png';
+import Task from './Task';
 import './Body.css';
 
 function Body() {
+	const [input, setInput] = useState('');
+
+	function handleChange(e) {
+		setInput(e.target.value);
+	}
+
 	return (
 		<main className='body'>
-			<div className='body__no-tasks'>
+			<div className='body__top'>
+				<form className='body__form'>
+					<input
+						type='text'
+						className='body__form-input'
+						value={input}
+						onChange={handleChange}
+						placeholder='Add a task...'
+					/>
+					<button type='submit' className='body__form-botton'>
+						Add Task
+					</button>
+				</form>
+				<div className='body__task-progress'>
+					<p>
+						Your progress: <span>0%</span>
+					</p>
+				</div>
+			</div>
+			{/* <div className='body__no-tasks'>
 				<img src={happy} className='body__image' alt='happy img' />
 				<h1>You have no task for now.</h1>
 				<p>You're on top of it! YOHOO</p>
+			</div> */}
+			<div className='body__tasks'>
+				<Task />
+				<Task />
+				<Task />
 			</div>
-			<div className='body__tasks'>{/* Task component */}</div>
 		</main>
 	);
 }
---
+++ src/components/Task.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Task.css';
+
+function Task() {
+	return <div>This is a task component</div>;
+}
+
+export default Task;
